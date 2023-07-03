b7b8e7b14b234a9b272f06d0d62e258379cddcdb
FrankGangWang <36325423+FrankGangWang@users.noreply.github.com>
2023-07-03 11:39:43 +0000

    adding initial layout of python project

---
+++ hello.py
@@ -0,0 +1,14 @@
+def more_hello():
+    return "hi"
+
+
+def more_bye():
+    return "bye"
+
+
+# this nonsense code is to test "make lint" to catch warning in code
+# var = 1
+# var = var
+
+# syntactical error
+# foo =
